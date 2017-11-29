7bdc5248c4e4d6d4773256dca01364b330e32c9d
lau-willis <lauwillis1991@gmail.com>
2017-11-29 11:38:03 -0500

    initial commit

---
+++ client/main.js
@@ -0,0 +1,16 @@
+//any JS is automatically ran here 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+	return (
+		<div>
+			React App #2
+		</div>
+		);
+};
+
+Meteor.startup(() => {
+	ReactDOM.render(<App />, document.querySelector('.container'));
+})
